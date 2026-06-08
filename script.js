5f8748d5b2eeba403cc613df2fc192eb157dca01
AarthyNaveenK <aarthysp321@gmail.com>
2026-06-08 19:19:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function updateCartLabel(){
- let c=JSON.parse(localStorage.getItem('cart')||'[]');
- let el=document.getElementById('cartLabel');
- if(el) el.innerText=c.length?`Cart (${c.length})`:'Filled with Emptiness Please fill me';
-}
-window.onload=updateCartLabel;
